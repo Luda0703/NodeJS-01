acce37680267fe0a695948d23143967a4d648693
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-07-28 21:58:43 +0300

    update

--- contacts.js
+++ contacts.js
@@ -1,32 +1,42 @@
 const fs = require('fs').promises;
 const path = require("path");
+const {nanoid} = require('nanoid');
 
+  const contactsPath = path.resolve("db", "contacts.json");
 
-//  Розкоментуй і запиши значення
-  // const contactsPath = path.resolve("db", "contacts.json");
-  const contactsPath = path.join(__dirname, "contacts.json");
-
-  // TODO: задокументувати кожну функцію
-const listContacts = async() => {
-    // const data = await fs.readFile(contactsPath);
-    const data = await fs.readFile(contactsPath);
-    console.log(data)
-  return JSON.parse(data); 
+  const listContacts = async() => {
+    const data = await fs.readFile(contactsPath); 
+    return JSON.parse(data); 
   }
  
-  
-  
   const getContactById = async(contactId) => {
-     // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+     const contacts = await listContacts();
+     const result = contacts.find(item => item.id === contactId);
+     return result || null;
   }
   
-  function removeContact(contactId) {
-    // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  const removeContact = async (contactId) => {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(item => item.id === contactId);
+    if(index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.readFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+    
   }
   
-  function addContact(name, email, phone) {
-    // ...твій код. Повертає об'єкт доданого контакту. 
+  const addContact = async(data) => {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      ...data,
+    }
+    contacts.push(newContact);
+    await fs.readFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
   }
 
-  module.exports = { listContacts };
+  module.exports = { listContacts, getContactById, addContact, removeContact };
  
--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-// const contacts = require("./contacts");
-const argv = require('yargs').argv;
+const contacts = require("./contacts");
 
-// console.log(contacts)
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
 const invokeAction = async ({action, id, name, email, phone}) => {
     switch(action) {
         case "list":
-            const allContacts = await contacts.listContacts;
+            const allContacts = await contacts.listContacts();
             return console.log(allContacts);
             break;
+        case 'get':
+            const oneContact = await contacts.getContactById(id);
+            return console.log(oneContact);
+            break;
+        case 'add':
+            const newContact = await contacts.addContact({name, email, phone});
+            return console.log(newContact);
+            break;
+        case 'remove':
+            const deletContact = await contacts.removeContact(id)
+            return console.log(deletContact);
+            break;
+
+            default:
+                console.warn('\x1B[31m Unknown action type!');
     }
 
 }
 
-// invokeAction({action: 'list'});
+// invokeAction({action: '"list"'});
+// invokeAction({action: 'get', id: "vza2RIzNGIwutCVCs4mCL"});
+// invokeAction({action: 'add', name: "Ludmila", email: "Luda@gmail.com", phone: '0993345098'});
+// invokeAction({action: 'remove', id: "vza2RIzNGIwutCVCs4mCL"});
 invokeAction(argv);
 
